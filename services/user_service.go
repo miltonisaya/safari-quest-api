4ef486fff48178ac46f507fb79711058cc5d2af6
Milton Isaya <miltonisaiah@gmail.com>
2026-06-24 22:58:00 +0300

    Rewrite user service to add role_ids to UserInput, resolve and validate role UUIDs before creation, hash password with bcrypt, and include roles in UserResponse DTO

--- services/user_service.go
+++ services/user_service.go
@@ -1,81 +1,151 @@
 package services
 
 import (
+	"fmt"
 	"time"
 
 	"safari-quest-api/models"
 	"safari-quest-api/repositories"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
-type RoleInput struct {
-	Name string `json:"name" binding:"required"`
-	Code string `json:"code" binding:"required"`
+type UserInput struct {
+	FirstName  string      `json:"first_name" binding:"required"`
+	MiddleName string      `json:"middle_name"`
+	LastName   string      `json:"last_name" binding:"required"`
+	Email      string      `json:"email" binding:"required,email"`
+	Password   string      `json:"password" binding:"required,min=8"`
+	Sex        string      `json:"sex" binding:"required"`
+	Mobile     string      `json:"mobile" binding:"required"`
+	Address    string      `json:"address" binding:"required"`
+	RoleIDs    []uuid.UUID `json:"role_ids" binding:"required,min=1"`
 }
 
-type RoleResponse struct {
-	UUID      uuid.UUID `json:"uuid"`
-	Name      string    `json:"name"`
-	Code      string    `json:"code"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+type UserResponse struct {
+	UUID            uuid.UUID      `json:"uuid"`
+	FirstName       string         `json:"first_name"`
+	MiddleName      string         `json:"middle_name"`
+	LastName        string         `json:"last_name"`
+	Email           string         `json:"email"`
+	Sex             string         `json:"sex"`
+	Mobile          string         `json:"mobile"`
+	Address         string         `json:"address"`
+	IsActive        bool           `json:"is_active"`
+	EmailVerifiedAt *time.Time     `json:"email_verified_at"`
+	Roles           []RoleResponse `json:"roles"`
+	CreatedAt       time.Time      `json:"created_at"`
+	UpdatedAt       time.Time      `json:"updated_at"`
 }
 
-func toRoleResponse(role models.Role) RoleResponse {
-	return RoleResponse{
-		UUID:      role.UUID,
-		Name:      role.Name,
-		Code:      role.Code,
-		CreatedAt: role.CreatedAt,
-		UpdatedAt: role.UpdatedAt,
+func toUserResponse(user models.User) UserResponse {
+	roles := make([]RoleResponse, len(user.Roles))
+	for i, role := range user.Roles {
+		roles[i] = toRoleResponse(role)
+	}
+	return UserResponse{
+		UUID:            user.UUID,
+		FirstName:       user.FirstName,
+		MiddleName:      user.MiddleName,
+		LastName:        user.LastName,
+		Email:           user.Email,
+		Sex:             user.Sex,
+		Mobile:          user.Mobile,
+		Address:         user.Address,
+		IsActive:        user.IsActive,
+		EmailVerifiedAt: user.EmailVerifiedAt,
+		Roles:           roles,
+		CreatedAt:       user.CreatedAt,
+		UpdatedAt:       user.UpdatedAt,
 	}
 }
 
-func RoleGetAll() ([]RoleResponse, error) {
-	roles, err := repositories.RoleFindAll()
+func resolveRoles(roleIDs []uuid.UUID) ([]models.Role, error) {
+	roles, err := repositories.RoleFindByUUIDs(roleIDs)
 	if err != nil {
 		return nil, err
 	}
-	response := make([]RoleResponse, len(roles))
-	for i, role := range roles {
-		response[i] = toRoleResponse(role)
+	if len(roles) != len(roleIDs) {
+		return nil, fmt.Errorf("one or more role_ids are invalid")
+	}
+	return roles, nil
+}
+
+func UserGetAll() ([]UserResponse, error) {
+	users, err := repositories.UserFindAll()
+	if err != nil {
+		return nil, err
+	}
+	response := make([]UserResponse, len(users))
+	for i, user := range users {
+		response[i] = toUserResponse(user)
 	}
 	return response, nil
 }
 
-func RoleGetByUUID(uuid uuid.UUID) (RoleResponse, error) {
-	role, err := repositories.RoleFindByUUID(uuid)
+func UserGetByUUID(uuid uuid.UUID) (UserResponse, error) {
+	user, err := repositories.UserFindByUUID(uuid)
 	if err != nil {
-		return RoleResponse{}, err
+		return UserResponse{}, err
 	}
-	return toRoleResponse(role), nil
+	return toUserResponse(user), nil
 }
 
-func RoleCreate(input RoleInput) (RoleResponse, error) {
-	role := models.Role{
-		Name: input.Name,
-		Code: input.Code,
-	}
-	if err := repositories.RoleCreate(&role); err != nil {
-		return RoleResponse{}, err
-	}
-	return toRoleResponse(role), nil
-}
-
-func RoleUpdate(uuid uuid.UUID, input RoleInput) (RoleResponse, error) {
-	role, err := repositories.RoleFindByUUID(uuid)
+func UserCreate(input UserInput) (UserResponse, error) {
+	roles, err := resolveRoles(input.RoleIDs)
 	if err != nil {
-		return RoleResponse{}, err
+		return UserResponse{}, err
 	}
-	role.Name = input.Name
-	role.Code = input.Code
-	if err := repositories.RoleUpdate(&role); err != nil {
-		return RoleResponse{}, err
+
+	hashed, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return UserResponse{}, err
 	}
-	return toRoleResponse(role), nil
+
+	user := models.User{
+		FirstName:  input.FirstName,
+		MiddleName: input.MiddleName,
+		LastName:   input.LastName,
+		Email:      input.Email,
+		Password:   string(hashed),
+		Sex:        input.Sex,
+		Mobile:     input.Mobile,
+		Address:    input.Address,
+		Roles:      roles,
+	}
+	if err := repositories.UserCreate(&user); err != nil {
+		return UserResponse{}, err
+	}
+	return toUserResponse(user), nil
 }
 
-func RoleDelete(uuid uuid.UUID) error {
-	return repositories.RoleDelete(uuid)
+func UserUpdate(uuid uuid.UUID, input UserInput) (UserResponse, error) {
+	user, err := repositories.UserFindByUUID(uuid)
+	if err != nil {
+		return UserResponse{}, err
+	}
+
+	roles, err := resolveRoles(input.RoleIDs)
+	if err != nil {
+		return UserResponse{}, err
+	}
+
+	user.FirstName = input.FirstName
+	user.MiddleName = input.MiddleName
+	user.LastName = input.LastName
+	user.Email = input.Email
+	user.Sex = input.Sex
+	user.Mobile = input.Mobile
+	user.Address = input.Address
+
+	if err := repositories.UserUpdate(&user, roles); err != nil {
+		return UserResponse{}, err
+	}
+	user.Roles = roles
+	return toUserResponse(user), nil
+}
+
+func UserDelete(uuid uuid.UUID) error {
+	return repositories.UserDelete(uuid)
 }
